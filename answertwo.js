2c16425171b960a92527c13275ab54ef872c5e14
TAYFUN KAYA <tayfunkaya.uni@gmail.com>
2022-01-10 22:03:16 +0300

    soru2

---
+++ answertwo.js
@@ -0,0 +1,19 @@
+
+
+
+
+function firstsinglechar(metin) {
+    var find;
+    for(var i=0;i<metin.length;i++){
+        var element = metin[i];
+        var index = metin.indexOf(element);
+        let result = metin.includes(element,index+1);
+        if(result==false){
+            find = element;
+            return find;
+        }
+    }
+    return "_"
+}
+var result  =  firstsinglechar("aabcbdde");
+console.log(result);
